591eecc69eeb1ad03703599b7a078778fdbcd03c
Joostlek <joostlek@outlook.com>
2025-03-09 12:25:35 +0100

    Refactor SSE

--- src/pysmartthings/smartthings.py
+++ src/pysmartthings/smartthings.py
@@ -181,7 +181,6 @@ class SmartThings:
     async def get_locations(self) -> list[BaseLocation]:
         """Retrieve SmartThings locations."""
         resp = await self._get("v1/locations")
-        LOGGER.warning("Locations: %s", resp)
         return LocationResponse.from_json(resp).items
 
     async def get_location(self, location_id: str) -> Location:
