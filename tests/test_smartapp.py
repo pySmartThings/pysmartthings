bfcfe407eaa3b4c370ff97f9574681cb911127f2
Joostlek <joostlek@outlook.com>
2025-11-24 17:48:05 +0100

    Add method to get installed app

--- tests/test_smartapp.py
+++ tests/test_smartapp.py
@@ -1,9 +1,11 @@
 """Tests for SmartThings SmartApp endpoints."""
 
-from aiohttp.hdrs import METH_DELETE
+from aiohttp.hdrs import METH_DELETE, METH_GET
 from aioresponses import aioresponses
+from syrupy import SnapshotAssertion
 
 from pysmartthings import SmartThings
+from . import load_fixture
 
 from .const import MOCK_URL, HEADERS
 
@@ -22,6 +24,32 @@ async def test_deleting_smart_app(
     responses.assert_called_once_with(
         f"{MOCK_URL}/v1/apps/c6cde2b0-203e-44cf-a510-3b3ed4706996",
         METH_DELETE,
+        headers={**HEADERS, "Authorization": "Bearer abcabcabcabc"},
+        params=None,
+        json=None,
+    )
+
+
+async def test_get_installed_app(
+    client: SmartThings,
+    responses: aioresponses,
+    snapshot: SnapshotAssertion,
+) -> None:
+    """Test getting an installed SmartApp."""
+    responses.get(
+        f"{MOCK_URL}/v1/installedapps/4514eb36-f5fd-4ab2-9520-0597acd1d212",
+        status=200,
+        body=load_fixture("installedapp_get_response.json"),
+    )
+    assert (
+        await client.get_installed_app(
+            "abcabcabcabc", "4514eb36-f5fd-4ab2-9520-0597acd1d212"
+        )
+        == snapshot
+    )
+    responses.assert_called_once_with(
+        f"{MOCK_URL}/v1/installedapps/4514eb36-f5fd-4ab2-9520-0597acd1d212",
+        METH_GET,
         headers={**HEADERS, "Authorization": "Bearer abcabcabcabc"},
         params=None,
         json=None,
